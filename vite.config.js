c440cfc463ffe0166e03449c7317ca8d613cc8e2
KACECRANDALL <119900862+KACECRANDALL@users.noreply.github.com>
2025-08-09 15:22:29 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: '/', // use '/<REPO_NAME>/' if NOT using a custom domain
 })
+
